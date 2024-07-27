b0bbdef696073a180b33c98b6f3f98fbd012bb1d
StalwartShalu <shaliniselvam098@gmail.com>
2024-07-27 21:54:33 +0530

    FNRC push

--- SamplePackage/FirstNonRepeatedCharacter.java
+++ SamplePackage/FirstNonRepeatedCharacter.java
@@ -1,10 +1,33 @@
 package SamplePackage;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FirstNonRepeatedCharacter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "NonCytoChemistry";
+		char[] charArray = str.toLowerCase().toCharArray();
+		int count =1;
+		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+		for(int i = 0 ; i<charArray.length; i++) {
+			if(! map.containsKey(charArray[i])) {
+				map.put(charArray[i], count);
+			}
+			else {
+				map.put(charArray[i], map.get(charArray[i])+1);
+			}
+		}
+		
+		for(Character ch: map.keySet()) {
+			if(map.get(ch)==1)  {
+				System.out.println("First Non Repeated Character is: " + ch);
+				break;
+			}
+		}
 	}
 
 }
+
+
+
